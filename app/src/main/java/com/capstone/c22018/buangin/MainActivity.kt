7a509d4d6110e3219121847e92e19107fa703cae
GGngodong <ikkoy123@gmail.com>
2022-11-04 14:11:05 +0700

    raw project

---
+++ app/src/main/java/com/capstone/c22018/buangin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.c22018.buangin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.c2218.buangin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
