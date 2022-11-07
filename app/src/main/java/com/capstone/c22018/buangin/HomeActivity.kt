11ac8fd01ec98bf15eecfafc928311fcae0462fa
Gededito <dito999dito@gmail.com>
2022-11-07 20:52:42 +0700

    home

--- app/src/main/java/com/capstone/c22018/buangin/HomeActivity.kt
+++ app/src/main/java/com/capstone/c22018/buangin/HomeActivity.kt
@@ -2,10 +2,15 @@ package com.capstone.c22018.buangin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.c22018.buangin.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/capstone/c22018/buangin/MainActivity.kt
+++ app/src/main/java/com/capstone/c22018/buangin/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.capstone.c22018.buangin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.c2218.buangin.R
+import android.os.Handler
+import android.os.Looper
+import com.capstone.c22018.buangin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+        }, 3000L)
+
     }
 }
