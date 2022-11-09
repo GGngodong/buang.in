5e4ec50bc13487a2a2115c7bb5178d8665efed72
GGngodong <ikkoy123@gmail.com>
2022-11-09 20:07:31 +0700

    added SplashScreenActivity.kt

---
+++ app/src/main/java/com/capstone/c22018/buangin/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.capstone.c22018.buangin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.capstone.c22018.buangin.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, OnBoardingActivity::class.java))
+            finish()
+        }, 3000L)
+    }
+}
