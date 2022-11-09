3b0ce0a74e8a63ff7cf94c2b52b872e39b84f65a
GGngodong <ikkoy123@gmail.com>
2022-11-09 21:30:02 +0700

    add RegisterActivity.kt

---
+++ app/src/main/java/com/capstone/c22018/buangin/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.c22018.buangin.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.c22018.buangin.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
