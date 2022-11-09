e21d3409bed72547e0ce2a8cb8fbb0083e747f93
GGngodong <ikkoy123@gmail.com>
2022-11-09 20:43:59 +0700

    add CustomView

---
+++ app/src/main/java/com/capstone/c22018/buangin/customview/EmailEditText.kt
@@ -0,0 +1,91 @@
+package com.capstone.c22018.buangin.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.Patterns
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.capstone.c22018.buangin.R
+import com.google.android.material.textfield.TextInputEditText
+
+class EmailEditText : TextInputEditText {
+
+    private lateinit var clearButton: Drawable
+
+    constructor(context: Context): super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr){
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        hint = ""
+        textAlignment = View.TEXT_ALIGNMENT_TEXT_START
+    }
+
+    private fun init(){
+        maxLines = 1
+        inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+
+        clearButton = ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+
+        addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int){
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int){
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+                if (s.toString().isNotEmpty()) showClearButton() else hideClearButton()
+
+                if (s.toString().isEmpty()) showError()
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(s).matches())
+                    error = "Email is not valid"
+
+            }
+        })
+    }
+
+    private fun showError(){
+        error = "Must be filled"
+    }
+
+    private fun showClearButton(){
+        setButtonDrawables(endOfTheText = clearButton)
+    }
+
+    private fun hideClearButton(){
+        setButtonDrawables()
+    }
+
+    private fun setButtonDrawables(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(
+            startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText
+        )
+    }
+}
---
+++ app/src/main/java/com/capstone/c22018/buangin/customview/PasswordEditText.kt
@@ -0,0 +1,120 @@
+package com.capstone.c22018.buangin.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.capstone.c22018.buangin.R
+import com.google.android.material.textfield.TextInputEditText
+
+class PasswordEditText : TextInputEditText, View.OnTouchListener {
+
+    private lateinit var eyeIcon: Drawable
+
+    constructor(context: Context): super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr){
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        showEyeButton()
+    }
+
+    private fun init(){
+        eyeIcon = ContextCompat.getDrawable(context, R.drawable.ic_eye) as Drawable
+
+        setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(S: CharSequence, start: Int, count: Int, after: Int){
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int){
+
+            }
+
+            override fun afterTextChanged(s: Editable){
+                if (s.toString().length < 6) showError()
+            }
+
+        })
+    }
+
+    private fun showError(){
+        error = "Password must be at least 6 characters"
+    }
+
+    private fun showEyeButton(){
+        setButtonDrawable(endOfTheText = eyeIcon)
+    }
+
+    private fun hideEyeButton(){
+        setButtonDrawable()
+    }
+
+    private fun setButtonDrawable(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(
+            startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText
+        )
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        if (compoundDrawables[2] != null) {
+            val eyeButtonStart: Float
+            val eyeButtonEnd: Float
+            var isEyeButtonClicked = false
+
+            if (layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                eyeButtonEnd = (eyeIcon.intrinsicWidth + paddingStart).toFloat()
+                if (event.x < eyeButtonEnd) isEyeButtonClicked = true
+            } else {
+                eyeButtonStart = (width - paddingEnd - eyeIcon.intrinsicWidth).toFloat()
+                if (event.x > eyeButtonStart) isEyeButtonClicked = true
+            }
+            if (isEyeButtonClicked) {
+                return when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        hideEyeButton()
+                        if (transformationMethod.equals(HideReturnsTransformationMethod.getInstance())) {
+                            transformationMethod = PasswordTransformationMethod.getInstance() // hide password
+                            eyeIcon = ContextCompat.getDrawable(context, R.drawable.ic_eye) as Drawable
+                            showEyeButton()
+                        } else {
+                            transformationMethod = HideReturnsTransformationMethod.getInstance() // show password
+                            eyeIcon = ContextCompat.getDrawable(context, R.drawable.ic_eye_off) as Drawable
+                            showEyeButton()
+                        }
+                        true
+                    }
+                    else -> false
+                }
+            } else return false
+        }
+        return false
+    }
+}
