3b4c7341ccfcc13b2685110e84c65f8542e67ca8
GGngodong <ikkoy123@gmail.com>
2022-11-12 19:48:17 +0700

    update JenisSampahActivity.kt

--- app/src/main/java/com/capstone/c22018/buangin/ui/jenis_sampah/JenisSampahActivity.kt
+++ app/src/main/java/com/capstone/c22018/buangin/ui/jenis_sampah/JenisSampahActivity.kt
@@ -2,6 +2,7 @@ package com.capstone.c22018.buangin.ui.jenis_sampah
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.c22018.buangin.R
 import com.capstone.c22018.buangin.databinding.ActivityJenisSampahBinding
 
 class JenisSampahActivity : AppCompatActivity() {
@@ -12,5 +13,14 @@ class JenisSampahActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityJenisSampahBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setToolBar()
+    }
+
+    private fun setToolBar(){
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setTitle(R.string.jenisSampah)
+        }
     }
 }
