c1652aa6486233057e667768fc516f7dc7574f7a
Gededito <dito999dito@gmail.com>
2022-12-02 19:55:34 +0700

    revisi5

--- app/src/main/java/com/capstone/c22018/buangin/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/capstone/c22018/buangin/ui/profile/ProfileActivity.kt
@@ -2,9 +2,7 @@ package com.capstone.c22018.buangin.ui.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.c22018.buangin.R
 import com.capstone.c22018.buangin.databinding.ActivityProfileBinding
-import com.capstone.c22018.buangin.ui.profile.profileuser.ProfileUserFragment
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -14,17 +12,6 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val mFragmentManager = supportFragmentManager
-        val mProfileUser = ProfileUserFragment()
-        val fragment = mFragmentManager.findFragmentByTag(ProfileUserFragment::class.java.simpleName)
-
-        if (fragment !is ProfileUserFragment) {
-            mFragmentManager
-                .beginTransaction()
-                .add(R.id.frame_profile, mProfileUser, ProfileUserFragment::class.java.simpleName)
-                .commit()
-        }
 
     }
 }
