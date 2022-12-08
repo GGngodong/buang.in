cbbca2e3fa6e7aa66e2934a16b9470af45560fef
Gededito <dito999dito@gmail.com>
2022-12-08 20:32:43 +0700

    registerPhotoError

--- app/src/main/java/com/capstone/c22018/buangin/ui/register/RegisterPhotoActivity.kt
+++ app/src/main/java/com/capstone/c22018/buangin/ui/register/RegisterPhotoActivity.kt
@@ -1,59 +1,124 @@
 package com.capstone.c22018.buangin.ui.register
 
 import android.Manifest
+import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
+import android.content.RestrictionsManager.RESULT_ERROR
 import android.content.pm.PackageManager
+import com.bumptech.glide.Glide.with
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.capstone.c22018.buangin.database.User
 import com.capstone.c22018.buangin.databinding.ActivityRegisterPhotoBinding
-import com.capstone.c22018.buangin.ui.login.LoginActivity
+import com.capstone.c22018.buangin.ui.home.MainActivity
+import com.capstone.c22018.buangin.utility.Preferences
 import com.capstone.c22018.buangin.utility.rotateBitmap
+import com.google.android.gms.cast.framework.media.ImagePicker
+import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import java.io.File
+import java.util.*
 
 class RegisterPhotoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterPhotoBinding
     private var getFile: File? = null
     private var result: Bitmap? = null
+    private var statusAdd: Boolean = false
+    private lateinit var filePath: Uri
+
+    private lateinit var storage: FirebaseStorage
+    private lateinit var storageReference: StorageReference
+    private lateinit var preferences: Preferences
+
+    private lateinit var user: User
+
+    private lateinit var mFirebaseDatabase: DatabaseReference
+    private lateinit var mFirebaseInstance: FirebaseDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterPhotoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        preferences = Preferences(this)
+        storage = FirebaseStorage.getInstance()
+        storageReference = storage.reference
+
+        mFirebaseInstance = FirebaseDatabase.getInstance()
+        mFirebaseDatabase = mFirebaseInstance.getReference("User")
+
+        binding.tvUsername.text = intent.getStringExtra("data")
+
         setActionBtn()
 
     }
 
     private fun setActionBtn() {
-
         binding.btnSimpan.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
-            Toast.makeText(this, "Foto Telah disimpan", Toast.LENGTH_SHORT).show()
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setTitle("Uploading...")
+            progressDialog.show()
+
+            val ref = storageReference.child("images/" + UUID.randomUUID().toString())
+            ref.putFile(filePath)
+                .addOnSuccessListener {
+                    progressDialog.dismiss()
+                    Toast.makeText(this, "Uploaded", Toast.LENGTH_SHORT).show()
+                    ref.downloadUrl.addOnSuccessListener {
+                        saveToFirebase(it.toString())
+                    }
+                }
+                .addOnFailureListener { e ->
+                    progressDialog.dismiss()
+                    Toast.makeText(this, "Failed " + e.message, Toast.LENGTH_SHORT).show()
+                }
+                .addOnProgressListener { taskSnapshot ->
+                    val progress = 100.0 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount
+                    progressDialog.setMessage("Uploaded " + progress.toInt() + "%")
+                }
+
         }
 
         binding.icAdd.setOnClickListener {
-           if (!allPermissionGranted()) {
-               ActivityCompat.requestPermissions(
-                   this,
-                   REQUIRED_PERMISSIONS,
-                   REQUEST_CODE_PERMISSIONS
-               )
-           } else {
-               startCameraX()
-           }
+            startCameraX()
         }
-
     }
 
+    private fun saveToFirebase(url: String) {
+        mFirebaseDatabase.child(user.name!!).addValueEventListener(object: ValueEventListener {
 
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                user.url = url
+                mFirebaseDatabase.child(user.name!!).setValue(user)
 
+                preferences.setValues("nama", user.name.toString())
+                preferences.setValues("email", user.email.toString())
+                preferences.setValues("url", "")
+                preferences.setValues("url", url)
+                preferences.setValues("status", "1")
+
+                val intent = Intent(this@RegisterPhotoActivity, MainActivity::class.java)
+                startActivity(intent)
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@RegisterPhotoActivity, ""+error.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
+    }
 
     private fun startCameraX() {
         val intent = Intent(this, CameraActivity::class.java)
